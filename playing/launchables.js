490703b4699c8f3212a0981d5365aded7fd316d9
fidgetsetc <kingdude75@outlook.com>
2026-07-18 20:21:50 -0500

    BIG BIG update
    Quick updates, beta, and duckmath update, plus new launchables

---
+++ playing/launchables.js
@@ -0,0 +1,26 @@
+// Apps
+const APPS = [
+  { id: 'app-duck', name: 'DuckMath', h2: 'Are the games not good enough? Here is a new site, but suggestions are recommended', img: './img/duck.avif', path: './html/duck/index.html' },
+  { id: 'app-chat', name: 'Chat', h2: 'Chat with many other users.', img: './img/chat.png', path: './html/chat/index.html' }, 
+  { id: 'app-deb', name: 'Debian Terminal', h2: 'A simulated Debian terminal! Familar to Debian, Ubuntu, Mint, Pi, and similar users.', img: './img/deb.png', path: './html/deb/index.html' }, 
+  { id: 'app-deb', name: 'Bouncing Distros', h2: 'Bouncing logos of Linux distros. We are not affiliated with any distros.', img: './img/ldb.png', path: './html/ldb/index.html' }, 
+
+];
+// Games
+const STATIC_GAMES = [
+  { id: 'game-brick', name: 'Brick', h2: 'Theres a ton of locked tablets. Guess youll toss bricks!', img: './img/brick.png', path: './html/brick/index.html?x=1' },
+  { id: 'game-bp', name: 'Bad Piggies', h2: 'Build contraptions to get to the end. Tip: Use cheats and use Field of Dreams to build almost anything', img: './img/bp.jpg', path: './html/bp/index.html' },
+  { id: 'game-ctr', name: 'Cut The Rope', h2: 'Cut some ropes and feed candy to Om Nom!', img: './img/ctr.jpg', path: './html/ctr/index.html' },
+  { id: 'game-sm', name: 'Slice Master', h2: 'Become the master of slicing.', img: './img/sm.jpg', path: './html/sm/index.html' },
+  { id: 'game-smb', name: 'Super Mario Bros', h2: 'Rescue Peach from Bowser and defeat other monsters on the way.', img: './img/smb.jpg', path: './html/smb/index.html' },
+  { id: 'game-fnf', name: 'FNF', h2: 'Can you keep up with the rhythm?', img: './img/fnf.jpg', path: './html/fnf/index.html' },
+  { id: 'game-fb', name: 'Flappy Bird', h2: 'Flap! Flap! Flap thru pipes!', img: './img/fb.webp', path: './html/fb/index.html' },
+  { id: 'game-bb', name: 'Block Blast', h2: 'Place blocks and match to edges.', img: './img/bb2.png', path: './html/bb/index.html' }, 
+  { id: 'game-au', name: 'Among Us', h2: 'The best web replica. Who is the imposter?! ', img: './img/au.jpeg', path: './html/au/index.html' },
+  { id: 'game-dd4', name: 'Droistroy 4', h2: 'Wreck Android 4. But not with malware!', img: './img/dd4.png', path: './html/dd4/index.html?x=1' },
+  { id: 'game-dd4', name: 'Incredibox Sprunk!', h2: 'This used to be popular. Not a fan of it but my brother likes it so why not. This has a feature to not have the hat.', img: './img/spr.jpeg', path: './html/spr/index.html?x=1' },
+];
+// Videos
+const VIDEOS = [
+  { id: 'vid-ab1', name: 'Angry Birds Movie', h2: 'Flightless birds lead a mostly happy existence, except for Red (Clay Kaytis), who just can not get past the daily annoyances of life. His temperament leads him to anger management class, where he meets fellow misfits Chuck (Fergal Reilly) and Bomb.', img: 'https://archive.org/download/the-angry-birds-movie-2016-upload-copy/maxresdefault.jpg', src: 'https://archive.org/download/the-angry-birds-movie-2016-upload-copy/The%20Angry%20Birds%20Movie%20%282016%29%20%284K%20HDR%20BluRay%20x265%20%2B%20Subtitles%29.mp4' },
+];
