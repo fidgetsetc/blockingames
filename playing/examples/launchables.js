35f21828716ac635fe41240bcbf5d91ba29b1691
fidgetsetc <kingdude75@outlook.com>
2026-07-24 13:47:49 -0500

    Some fixes and refinements!

---
+++ playing/examples/launchables.js
@@ -0,0 +1,13 @@
+// Apps
+const APPS = [
+  { id: 'app-0', name: 'placeholder', h2: 'test', img: '', path: '' },
+
+];
+// Games
+const STATIC_GAMES = [
+  { id: 'game-0', name: 'placeholder', h2: 'test', img: '', path: '' },
+];
+// Videos
+const VIDEOS = [
+  { id: 'video-0', name: 'placeholder', h2: 'test', img: '', src: '' },
+];
---
+++ playing/examples/themes.js
@@ -0,0 +1,4 @@
+// Theme shop catalog with prepended './playing/' subdirectories for valid referencing
+const STORE_ITEMS = [
+  { id: 'bg-placeholder',        name: 'test',                      type: 'background',     value: 'url() no-repeat center center / cover', cost: 1, img: '' }
+];
